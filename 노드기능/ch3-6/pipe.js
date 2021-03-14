b98889df5cefae27ddb7c62d12e2a8ec55589ae7
hojun <mmmhobin3@gmail.com>
2021-03-15 02:03:39 +0900

    node

--- 노드기능/ch3-6/pipe.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const zlib = require('zlib');
-
-const readStream = fs.createReadStream('./readme3.txt', {highWaterMark :16 });
-const zlibStream = zlib.createGzip();
-const writeStream = fs.createWriteStream('./write3.txt');
-readStream.pipe(writeStream);
- 
-readStream.pipe(zliStream).pipe(writeStream);
